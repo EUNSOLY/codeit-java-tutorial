09a15c9d25431acea7337b85c9f793c1488bc3ac
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 13:30:57 +0900

    실습 2-23: @EqualsAndHashCode - exclude 옵션 : 비교안할 필드 제외

--- src/main/java/com/example/demo/LombokExample.java
+++ src/main/java/com/example/demo/LombokExample.java
@@ -4,8 +4,8 @@ package com.example.demo;
 public class LombokExample {
 
     public static void main(String[] arge) {
-        PersonLombok person = new PersonLombok(1, "Aaron", 10, "aaron@example.com");
-        PersonLombok person2 = new PersonLombok(1, "Aaron", 10, "aaron@example.com");
+        PersonLombok person = new PersonLombok(1, "Aaron", 12, "aaron@example.com");
+        PersonLombok person2 = new PersonLombok(1, "Aaron", 24, "aaron@different.com");
         System.out.println("---");
         System.out.println(person);              // 객체 저장 주소
         System.out.println(person.toString());   // 객체 저장 주소
--- src/main/java/com/example/demo/PersonLombok.java
+++ src/main/java/com/example/demo/PersonLombok.java
@@ -5,7 +5,7 @@ import lombok.*;
 @Getter
 @Setter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
-@EqualsAndHashCode
+@EqualsAndHashCode(exclude = {"age", "email"})
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class PersonLombok {
     @ToString.Include(name = "memberId")
