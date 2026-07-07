638ab8cc5e11636e497de6644348bf2194719f7c
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 20:36:26 +0900

    실습 2-31: @DaTa - 주의 : @ALL 이 아닌 @requiredargsconstructor 추가됨

--- src/main/java/com/example/demo/ano/Example.java
+++ src/main/java/com/example/demo/ano/Example.java
@@ -7,25 +7,19 @@ import java.util.List;
 
 public class Example {
     public static void main(String[] arge) {
-
-
-        Member aaron = Member.getInstance();
-        Member baron = Member.getInstance();
-        System.out.println("---");
-        System.out.println(aaron);              // 객체 주소 (aaron 주소값 == baron 주소값)
-
-
-//      System.out.println(aaron.toString());   // 객체
-//      System.out.println(aaron.getName());    // 필드 중 name
-//      System.out.println(aaron.name);         // 필드 중 name (접근제어자 private)
+        Administrator aaron = new Administrator(1, "Aaron", 12, "aaron@example.com", "DEVELOPER");
+        Administrator baron = new Administrator(2, "Baron", 36, "baron@example.com", "DEVELOPER");
 
         System.out.println("---");
-        System.out.println(baron);              // 객체 주소 (aaron 주소값 == baron 주소값)
-//      System.out.println(baron.toString());   // 객체
-//      System.out.println(baron.getName());    // 필드 중 name
-//      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
+        System.out.println(aaron);              // 객체
+        System.out.println(aaron.toString());   // 객체
+
+        System.out.println("---");
+        System.out.println(baron);              // 객체
+        System.out.println(baron.toString());   // 객체
 
         System.out.println("---");
         System.out.println(aaron == baron);
+        System.out.println(aaron.equals(baron));
     }
 }
--- src/main/java/com/example/demo/ano/Member.java
+++ src/main/java/com/example/demo/ano/Member.java
@@ -2,24 +2,22 @@ package com.example.demo.ano;
 
 import com.example.demo.dto.MemberCreateRequestDto;
 import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.util.List;
 import java.util.Objects;
 
 
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+//@Getter
+//@Setter
+//@ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
+//@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+//@AllArgsConstructor(access = AccessLevel.PUBLIC)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
 public class Member {
-
-    private static class LazyHolder {
-        private static final Member UNIQUE_INSTANCE = new Member();
-    }
-
-    protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
-    protected String name;
-    protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
-    protected String email;
-
-    public static Member getInstance() {
-        return LazyHolder.UNIQUE_INSTANCE;
-    }
+    /* private */ Integer id;
+    /* private */ String name;
+    /* private */ int age;
+    /* private */ String email;
 }
