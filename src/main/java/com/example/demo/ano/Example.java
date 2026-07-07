91b41fc2f71a165b4c492363a16ac335cb8be993
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 18:33:14 +0900

    실습 1-16: 빌더(@builder) 통한 객체 생성 장점 - (3) 필드 설정 시점/역할 분리 가능

--- src/main/java/com/example/demo/ano/Example.java
+++ src/main/java/com/example/demo/ano/Example.java
@@ -3,19 +3,23 @@ package com.example.demo.ano;
 
 public class Example {
     public static void main(String[] arge) {
-        Member aaron = Member.builder()
-                .id(1)
-                .age(10)
-                .name("Aaron")
-                .email("aaron@example.com")
-                .build();
+        String request = "AARON";
 
-        Member baron = Member.builder()
-                .email("baron@example.com")
-                .name("Baron")
-                .age(20)
-                .id(2)
-                .build();
+        // (1) 빌더 정의
+        Member.MemberBuilder builder = Member.builder();
+        // (2) 필드 설정 : (2.1) 공통 부분을 먼저 설정하고
+        builder.age(1)
+                .email("common@example.com");
+
+        // (2) 필드 설정 : (2.2) 다른 부분은 따로 설정한다 (조건부)
+        if (request.equals("AARON")) {
+            builder.id(1).name("Aaron");
+        } else if (request.equals("BARON")) {
+            builder.id(2).name("Baron");
+        }
+
+        //(3) 객체 생성
+        Member aaron = builder.build();
 
         System.out.println("---");
         System.out.println(aaron);              // 객체
@@ -23,12 +27,6 @@ public class Example {
         System.out.println(aaron.getName());    // 필드 중 name
 //      System.out.println(aaron.name);
 
-
-        System.out.println("---");
-        System.out.println(baron);              // 객체
-        System.out.println(baron.toString());   // 객체
-        System.out.println(baron.getName());    // 필드 중 name
-//      System.out.println(baron.name);         // 필드 중 name (접근제어자 private)
 
     }
 }
