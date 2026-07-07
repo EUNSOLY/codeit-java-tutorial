c4b4f071ea760d30d8f4845f9c3ed9aceb502e6b
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 12:58:35 +0900

    실습 2-9: @ToString.Exclude : 미출력 필드 제외

--- src/main/java/com/example/demo/PersonLombok.java
+++ src/main/java/com/example/demo/PersonLombok.java
@@ -3,14 +3,15 @@ package com.example.demo;
 import lombok.*;
 
 @Getter
-@ToString(exclude = {"age", "email"})
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class PersonLombok {
     private Integer id;
     private String name;
     @Setter(AccessLevel.PRIVATE)
+    @ToString.Exclude
     private int age;
     @Setter
+    @ToString.Exclude
     private String email;
 
 
