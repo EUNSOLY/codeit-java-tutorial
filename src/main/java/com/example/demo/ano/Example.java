fe962108a747a783c420863f248cf881e36607e7
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 18:05:44 +0900

    실습 1-11: DTO(Data Transfer Object)를 생성자로 받아 필요한 필드만 사용

--- src/main/java/com/example/demo/ano/Example.java
+++ src/main/java/com/example/demo/ano/Example.java
@@ -3,17 +3,13 @@ package com.example.demo.ano;
 
 public class Example {
     public static void main(String[] arge) {
-        Member aaron = new Member("Aaron");
-
-        aaron.setEmail("aaron@example.com");
-        Member baron = new Member("Baron");
-
-        Member baron = new Member();
-        baron.setName("Baron");
-        baron.setEmail("baron@example.com");
+        MemberRequestDto dto = new MemberRequestDto("Aaron", "aaron@example.com");
+        Member aaron = new Member(dto);
 
         System.out.println("---");
-        System.out.println(baron.toString());   // 객체
-        System.out.println(baron.getName());    // 필드 중 name
+        System.out.println(aaron);   // 객체
+        System.out.println(aaron.toString());   // 객체
+        System.out.println(aaron.getName());    // 필드 중 name
+        //      System.out.aaron(baron.name)
     }
 }
