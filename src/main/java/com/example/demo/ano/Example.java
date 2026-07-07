57cc617430bea9c219b216dd5f3c1d62995f1241
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 19:15:30 +0900

    실습 1-19: (심화) 빌더(@builder) 사용 시 - (3) Collection 내 단일 요소 설정 @Singular

--- src/main/java/com/example/demo/ano/Example.java
+++ src/main/java/com/example/demo/ano/Example.java
@@ -1,18 +1,23 @@
 package com.example.demo.ano;
 
 
+import java.util.List;
+
 public class Example {
     public static void main(String[] arge) {
 
         Member aaron = Member.builder()
                 .name("Aaron")
                 .email("aaron@example.com")
-                .build();
-        Member baron = Member.builder()
-//              .name("Unnamed")       // 빌더 사용 시 .name() 에 어떤값도 넣지 않으면 name 필드에는 "Unnamed" 가 설정된다.
-//              .email("Undefined")     // 빌더 사용 시 .email() 에 어떤값도 넣지 않으면 name 필드에는 "Undefined" 가 설정된다.
+                .favorites(List.of("Game", "Animation"))
                 .build();
 
+        Member baron = Member.builder()
+                .name("Baron")
+                .email("baron@example.com")
+                .favorite("Book")
+                .favorite("Cook")
+                .build();
 
         System.out.println("---");
         System.out.println(aaron);              // 객체
--- src/main/java/com/example/demo/ano/Member.java
+++ src/main/java/com/example/demo/ano/Member.java
@@ -2,6 +2,8 @@ package com.example.demo.ano;
 
 import lombok.*;
 
+import java.util.List;
+
 @Builder
 public class Member {
     protected Integer id;     // Reference Type 주소를 담는 그릇 : 객체가 담기며, NULL 이 들어갈 수 있다.
@@ -10,10 +12,11 @@ public class Member {
     protected int age;        // Primitive Type 값을 담는 그릇 : 값 그 자체가 담기며, NULL 이 들어갈 수 없다. (무조건 값이 있어야하기에 기본값 존재)
     @Builder.Default
     protected String email = "Undefined";
-    g
+    @Singular
+    protected List<String> favorites;
 
     public String toString() {
-        return String.format("Member(id=%s, name=%s, age=%s, email=%s)", id, name, age, email);
+        return String.format("Member(id=%s, name=%s, age=%s, email=%s, favorites=%s)", id, name, age, email, favorites);
     }
 
     public String getName() {
