24be76336986097afa78dcdcc51553988b961e75
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 20:44:41 +0900

    실습 2-32: @DaTa - 주의 : Final/Non-Final 필드 구별하여 생성자/수정자 호출

--- src/main/java/com/example/demo/ano/Member.java
+++ src/main/java/com/example/demo/ano/Member.java
@@ -1,23 +1,13 @@
 package com.example.demo.ano;
 
-import com.example.demo.dto.MemberCreateRequestDto;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.util.List;
-import java.util.Objects;
-
-
-//@Getter
-//@Setter
-//@ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
-//@EqualsAndHashCode(onlyExplicitlyIncluded = true)
-//@AllArgsConstructor(access = AccessLevel.PUBLIC)
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 @Data
 public class Member {
-    /* private */ Integer id;
-    /* private */ String name;
-    /* private */ int age;
-    /* private */ String email;
+    /* private final */ Integer id;
+    /* private final */ String name;
+    /* private final */ int age;
+    /* private final */ String email;
 }
