bc8032bfb90e80d2afc8c040ba3e85c6f73097db
EUNSOLY <dmsthf9596@gmail.com>
2026-07-07 11:56:28 +0900

    실습 2-4: @Getter - 클래스 내 필드 단위 적용

--- src/main/java/com/example/demo/LombokExample.java
+++ src/main/java/com/example/demo/LombokExample.java
@@ -10,6 +10,12 @@ public class LombokExample {
         System.out.println(person);              // 객체 저장 주소
         System.out.println(person.toString());   // 객체 저장 주소
 
+
+//      person.setId();                          // @Setter 필드 단위로 설정되어있지 않아 필드 수정 불가능
+//      person.setName();                        // @Setter 필드 단위로 설정되어있지 않아 필드 수정 불가능
+        person.setAge(20);                       // @Setter (필드 단위)
+        person.setEmail("aaron@different.com");  // @Setter (필드 단위)
+
         System.out.println(person.getId());      // @Getter 클래스 단위로 적용하여 모든 필드 조회 가능
         System.out.println(person.getName());    // @Getter (클래스 단위)
         System.out.println(person.getAge());     // @Getter (클래스 단위)
--- src/main/java/com/example/demo/PersonLombok.java
+++ src/main/java/com/example/demo/PersonLombok.java
@@ -3,13 +3,16 @@ package com.example.demo;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 @Getter
 public class PersonLombok {
     private Integer id;
     private String name;
+    @Setter
     private int age;
+    @Setter
     private String email;
 
 
