92625fa2ea7c96fdc1a719297e3358e257c1c716
EUNSOLY <dmsthf9596@gmail.com>
2026-07-08 10:51:12 +0900

    실습 1-32: Static 메서드 - 객체 생성 후 호출하지 않고 클래스로 바로 호출

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,9 +8,8 @@ public class DemoApplication {
         Member member = new Member(1, "Aaron", 10, "aaron@example.com");
 
         System.out.println("----");
-        System.out.println(member);
-        System.out.println(member.toString());
-        System.out.println(member.MEMBER_TO_STRING_FORMAT); // * 앞글자 소문자 : 물론 이렇게 호출할 수 있겠지만, 정상사용은 아님
+        System.out.println(member.toString(member));        // * 앞글자 소문자 : 물론 이렇게 호출할 수 있겠지만, 정상사용은 아님        System.out.println(member.toString(member));
+        System.out.println(Member.toString(member));        // * 앞글자 대문자 : Static 메서드는 이렇게 호출하는것이 정상이다.g        System.out.println(member.MEMBER_TO_STRING_FORMAT); // * 앞글자 소문자 : 물론 이렇게 호출할 수 있겠지만, 정상사용은 아님
         System.out.println(Member.MEMBER_TO_STRING_FORMAT); // * 앞글자 대문자 : Static 필드는 이렇게 호출하는것이 정상이다.
     }
 }
--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -14,8 +14,8 @@ public class Member {
     private String email;
 
 
-    public String toString() {
-        return String.format(MEMBER_TO_STRING_FORMAT, this.id, this.name, this.age, this.email);
+    public static String toString(Member member) {
+        return String.format(MEMBER_TO_STRING_FORMAT, member.id, member.name, member.age, member.email);
     }
 }
 
