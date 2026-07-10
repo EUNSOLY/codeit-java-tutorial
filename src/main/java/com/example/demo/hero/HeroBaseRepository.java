f50ccad137f023344465dfb9948d69c4e74f22e0
EUNSOLY <dmsthf9596@gmail.com>
2026-07-10 17:52:30 +0900

    실습7. 삭제 메서드 streamAPI 사용 버전으로 변경
    - developer-talk.tistory.com/707 티스토리 보고 StreamAPI 사용
    - 대애충 흐름을 이해함 StreamAPI에 대한 이해도 낮음

--- src/main/java/com/example/demo/hero/HeroBaseRepository.java
+++ src/main/java/com/example/demo/hero/HeroBaseRepository.java
@@ -42,25 +42,30 @@ public abstract class HeroBaseRepository implements HeroRepository {
     public void delete(String name) {
 
         if (Objects.nonNull(findByName(name))) {
-            // Arrays.copyOfRange()
-            // 원본배열, 복사를 시작할 원본 배열의 인덱스, 복사 대상 배열, 복사를 시작할 대상 배열의 인덱스, 복사할 요소의 개수
-            // System.arraycopy();
-            // 원본배열, 복사를 시작할 원본 배열의 인덱스, 복사 대상 배열, 복사를 시작할 대상 배열의 인덱스, 복사할 요소의 개수
-            for (int i = 0; i < this.heroes.length; i++) {
-                if (this.heroes[i].getName().equals(name)) {
-                    int position = i == 0 ? 0 : i - 1;
-                    Hero[] arr1 = Arrays.copyOfRange(this.heroes, 0, position); // 0부터 position 앞 요소까지만 복사
-                    Hero[] arr2 = Arrays.copyOfRange(this.heroes, i + 1, this.heroes.length); // position 부터 마지막 요소까지 복사(길이의 앞 요소는 마지막 요소)
-                    Hero[] result = new Hero[arr1.length + arr2.length]; // 쪼개기한 배열을 합산 길이로 신규 배열 생성
-                    System.arraycopy(arr1, 0, result, 0, arr1.length); // 앞쪽
-                    System.arraycopy(arr2, arr1.length, result, arr1.length, arr2.length); // 뒷쪽
-                    this.heroes = result;
-                    return;
-                }
-
-
-            }
+            this.heroes = Arrays.stream(this.heroes)
+                    .filter(h -> !h.getName().equals(name))
+                    .toArray(Hero[]::new);
+            return;
         }
+//        if (Objects.nonNull(findByName(name))) {
+
+        // Arrays.copyOfRange()
+        // 원본배열, 복사를 시작할 원본 배열의 인덱스, 복사 대상 배열, 복사를 시작할 대상 배열의 인덱스, 복사할 요소의 개수
+        // System.arraycopy();
+        // 원본배열, 복사를 시작할 원본 배열의 인덱스, 복사 대상 배열, 복사를 시작할 대상 배열의 인덱스, 복사할 요소의 개수
+//            for (int i = 0; i < this.heroes.length; i++) {
+//                if (this.heroes[i].getName().equals(name)) {
+//                    int position = i == 0 ? 0 : i - 1;
+//                    Hero[] arr1 = Arrays.copyOfRange(this.heroes, 0, position); // 0부터 position 앞 요소까지만 복사
+//                    Hero[] arr2 = Arrays.copyOfRange(this.heroes, i + 1, this.heroes.length); // position 부터 마지막 요소까지 복사(길이의 앞 요소는 마지막 요소)
+//                    Hero[] result = new Hero[arr1.length + arr2.length]; // 쪼개기한 배열을 합산 길이로 신규 배열 생성
+//                    System.arraycopy(arr1, 0, result, 0, arr1.length); // 앞쪽
+//                    System.arraycopy(arr2, arr1.length, result, arr1.length, arr2.length); // 뒷쪽
+//                    this.heroes = result;
+//                    return;
+//                }
+//            }
+//        }
 
 
         throw new RuntimeException("삭제할 영웅이 없습니다. : " + name);
