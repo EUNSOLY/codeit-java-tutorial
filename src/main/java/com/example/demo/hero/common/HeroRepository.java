f1dba450c66b8f2424d0c61ded4b0004df56ad0d
EUNSOLY <dmsthf9596@gmail.com>
2026-07-10 15:34:20 +0900

    레포 메서드 임시 생성

--- src/main/java/com/example/demo/hero/common/HeroRepository.java
+++ src/main/java/com/example/demo/hero/common/HeroRepository.java
@@ -6,6 +6,14 @@ public interface HeroRepository {
     // 영웅 이름을 기반으로 영웅을 찾아서 반환한다.
     Hero findByName(String name);
 
+    default void create(String name) {
+        throw new RuntimeException("생성 메서드가 없습니다. 생성해주세요");
+    }
+
+    default void update(Hero hero) {
+        throw new RuntimeException("업데이트 메서드가 없습니다. 생성해주세요");
+    }
+
     default void delete(String name) {
         throw new RuntimeException("삭제 메서드가 없습니다. 생성해주세요");
     }
