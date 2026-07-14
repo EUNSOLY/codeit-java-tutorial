70e394d7626e90dd9bb160e6cc297007053b0315
EUNSOLY <dmsthf9596@gmail.com>
2026-07-14 11:35:40 +0900

    실습1.  Collection 1) Array<Element> - 길이 / 값 기반 선언
    - Array는 Collection은 아니지만 다른점을 비교하기 위해서 체크

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,28 @@
 package com.example.demo;
 
 
+import com.example.demo.member.Member;
+
 //@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
+        int[] int_array = new int[3];                        // 길이 기반 Array 초기화 (기본형 타입)
+        int[] int_array2 = {1, 2, 3};                        // 값 기반 Array 초기화 (기본형 타입)
+        Integer[] integer_array = new Integer[3];            // 길이 기반 Array 초기화 (참조형 타입)
+        Integer[] integer_array2 = new Integer[]{1, 2, 3, 4, 5}; // 값 기반 Array 초기화 (참조형 타입)
+
+        System.out.println(int_array.length);
+        System.out.println(int_array2.length);
+        System.out.println(integer_array.length);
+        System.out.println(integer_array2.length);
+
+        Member[] members = {
+                new Member(1, "Aaron", 10, ""),
+                new Member(2, "Baron", 10, ""),
+                new Member(3, "Caron", 10, ""),
+                new Member(4, "Daron", 10, "")
+        };
+
 
     }
 
