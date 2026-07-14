89936022aa214bb405dcfc11dad6cd66fefd614f
EUNSOLY <dmsthf9596@gmail.com>
2026-07-14 13:15:54 +0900

    실습3. Collection 3) Set<Element> - 빈(Empty) / 값 기반 선언
    Arrays.asList로 데이터를 제공했지만 clear등 메서드 사용가능
    이는 단순 데이터를 주입하기위해 사용됬을 뿐 HashSet 내부에 주입됬기 때문에 HashSet의 성질을 갖고있다.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,32 +1,50 @@
 package com.example.demo;
 
-
-import com.example.demo.member.Member;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 
 //@SpringBootApplication
 public class DemoApplication {
     public static void main(String[] args) {
-        int[] int_array = new int[3];                        // 길이 기반 Array 초기화 (기본형 타입)
-        int[] int_array2 = {1, 2, 3};                        // 값 기반 Array 초기화 (기본형 타입)
-        Integer[] integer_array = new Integer[3];            // 길이 기반 Array 초기화 (참조형 타입)
-        Integer[] integer_array2 = new Integer[]{1, 2, 3, 4, 5}; // 값 기반 Array 초기화 (참조형 타입)
+        /* Collection 3) Set 세트 */
+
+        //      Set<String> string_set = new HashSet<>();                  // 빈 Set 선언 (String 요소)
+        //      string_set.add("1");
+        //      string_set.add("2");
+        //      string_set.add("3");
+        Set<String> string_set = new HashSet<>(Arrays.asList("1", "2", "3"));
+
+        string_set.add("4");                        // 추가
+        string_set.remove("1");                  // 삭제
+        string_set.contains("1");                   // 포함
+        string_set.clear();                         // 리셋
+        string_set.isEmpty();                       // 검사
+        System.out.println(string_set.size());
+
+        Set<Integer> integer_set = new HashSet<>(Arrays.asList(1, 2, 3));
+        integer_set.add(4);                         // 추가
+        integer_set.remove(3);                      // 삭제
+        integer_set.contains(1);                    // 포함
+        integer_set.clear();                        // 리셋
+        integer_set.isEmpty();                      // 검사
+        integer_set.size();                         // 개수
+
+//        int[] int_array = new int[3];                        // 길이 기반 Array 초기화 (기본형 타입)
+//        int[] int_array2 = {1, 2, 3};                        // 값 기반 Array 초기화 (기본형 타입)
+//        Integer[] integer_array = new Integer[3];            // 길이 기반 Array 초기화 (참조형 타입)
+//        Integer[] integer_array2 = new Integer[]{1, 2, 3, 4, 5}; // 값 기반 Array 초기화 (참조형 타입)
 
 //        System.out.println(int_array.length);
 //        System.out.println(int_array2.length);
 //        System.out.println(integer_array.length);
 //        System.out.println(integer_array2.length);
 
-        Member[] members = {
-                new Member(1, "Aaron", 10, ""),
-                new Member(2, "Baron", 10, ""),
-                new Member(3, "Caron", 10, ""),
-                new Member(4, "Daron", 10, "")
-        };
+//        Member[] members = {
+//                new Member(1, "Aaron", 10, ""),
+//                new Member(2, "Baron", 10, ""),
+//                new Member(3, "Caron", 10, ""),
+//                new Member(4, "Daron", 10, "")
+//        };
 
 
         /* Collection 2 List */
@@ -37,44 +55,44 @@ public class DemoApplication {
 //        string_list.add("3");
 
 
-        List<String> string_list = Arrays.asList("1", "2", "3");
-        System.out.println(string_list.get(2)); // 3
+//        List<String> string_list = Arrays.asList("1", "2", "3");
+//        System.out.println(string_list.get(2)); // 3
 //      string_list.add("4");           // 추가 : java.lang.UnsupportedOperationException 에러 발생 (고정길이 배열로 만든 List에 값을 추가하려고 했기 때문)
 //      string_list.remove(2);          //  삭제 : java.lang.UnsupportedOperationException 에러 발생 (고정길이 배열로 만든 List에 값을 추가하려고 했기 때문)
 //      string_list.clear();            // 리셋 : 오류
-        string_list.set(2, "5");
-        System.out.println(string_list.get(2));  // 5
+//        string_list.set(2, "5");
+//        System.out.println(string_list.get(2));  // 5
 
-        List<Integer> integet_array = Arrays.asList(1, 2, 3); // 고정길이 List 선언 (Arrays는 고정 배열로 asList를 통해 List화 시켰지만 길이는 고정임)
-        List<Integer> integer_list = new ArrayList<>();
-
-        integet_array.get(1);                         // 조회
-        integet_array.set(1, 5);
-        integet_array.sort(Integer::compareTo);
-        integet_array.set(1, 5);                    // 수정
-        integet_array.subList(0, 1);                // 부분
-        integet_array.sort(Integer::compareTo);     // 나열
-        integet_array.contains(4);                  // 포함
-
-        integet_array.isEmpty();                    // 검사
-        integet_array.size();
-        System.out.println(integet_array);
-
-        integer_list.add(1);
-        integer_list.add(2);
-        integer_list.add(3);
-        integer_list.add(4);
-
-        integer_list.get(3);                        // 조회
-        integer_list.add(4);                        // 추가
-        integer_list.set(3, 5);                     // 수정
-        integer_list.remove(3);               // 삭제
-        integer_list.subList(0, 1);                 // 부분
-        integer_list.sort(Integer::compareTo);      // 나열
-        integer_list.contains(1);                   // 포함
-        integer_list.clear();                       // 리셋
-        integer_list.isEmpty();                     // 검사
-        integer_list.size();                        // 개수
+//        List<Integer> integet_array = Arrays.asList(1, 2, 3); // 고정길이 List 선언 (Arrays는 고정 배열로 asList를 통해 List화 시켰지만 길이는 고정임)
+//        List<Integer> integer_list = new ArrayList<>();
+//
+//        integet_array.get(1);                         // 조회
+//        integet_array.set(1, 5);
+//        integet_array.sort(Integer::compareTo);
+//        integet_array.set(1, 5);                    // 수정
+//        integet_array.subList(0, 1);                // 부분
+//        integet_array.sort(Integer::compareTo);     // 나열
+//        integet_array.contains(4);                  // 포함
+//
+//        integet_array.isEmpty();                    // 검사
+//        integet_array.size();
+//        System.out.println(integet_array);
+//
+//        integer_list.add(1);
+//        integer_list.add(2);
+//        integer_list.add(3);
+//        integer_list.add(4);
+//
+//        integer_list.get(3);                        // 조회
+//        integer_list.add(4);                        // 추가
+//        integer_list.set(3, 5);                     // 수정
+//        integer_list.remove(3);               // 삭제
+//        integer_list.subList(0, 1);                 // 부분
+//        integer_list.sort(Integer::compareTo);      // 나열
+//        integer_list.contains(1);                   // 포함
+//        integer_list.clear();                       // 리셋
+//        integer_list.isEmpty();                     // 검사
+//        integer_list.size();                        // 개수
 
 
     }
