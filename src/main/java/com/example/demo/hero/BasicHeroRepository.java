7ab277bc88cebe5823bb0d3893ada58703adb56f
EUNSOLY <dmsthf9596@gmail.com>
2026-07-14 14:06:36 +0900

    실습5. Array -> List로 치환

--- src/main/java/com/example/demo/hero/BasicHeroRepository.java
+++ src/main/java/com/example/demo/hero/BasicHeroRepository.java
@@ -3,14 +3,15 @@ package com.example.demo.hero;
 import com.example.demo.hero.common.CrudRepository;
 import com.example.demo.hero.common.Hero;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public abstract class BasicHeroRepository<T extends Hero> implements CrudRepository<T> {
-    private T[] heroes;
+    private List<T> heroes = new ArrayList<>();
 
-    public BasicHeroRepository(T[] heroes) {
-        this.heroes = heroes;
+    public BasicHeroRepository(List<T> heroes) {
+        this.heroes.addAll(heroes);
     }
 
     @Override
@@ -27,11 +28,18 @@ public abstract class BasicHeroRepository<T extends Hero> implements CrudReposit
     public void create(T hero) {
         if (Objects.isNull(this.findHeroByName(hero.getName()))) {
             // null이 맞다면
-            T[] newHeroes = Arrays.copyOf(this.heroes, this.heroes.length + 1);
-            newHeroes[newHeroes.length - 1] = hero;
-            this.heroes = newHeroes;
+            this.heroes.add(hero);
         } else {
             throw new RuntimeException("이미 존재하는 영웅의 이름입니다! 생성한 영웅 이름 : " + hero.getName());
         }
+    }
+
+    @Override
+    public void delete(String name) {
+        T retrieve = findByName(name);
+        if (Objects.isNull(retrieve)) {
+            throw new RuntimeException("지우시려는 이름의 영웅이 기존에 없습니다! - 받은 영웅 객체의 이름 : " + name);
+        }
+        this.heroes.remove(retrieve);
     }
 }
--- src/main/java/com/example/demo/hero/CacheBasicHeroRepository.java
+++ src/main/java/com/example/demo/hero/CacheBasicHeroRepository.java
@@ -4,12 +4,14 @@ import com.example.demo.hero.common.CrudRepository;
 import com.example.demo.hero.common.Hero;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public abstract class CacheBasicHeroRepository<T extends Hero> implements CrudRepository<T> {
     // 서버 내 캐시라고 가정 : (1) 조회에 드는 시간이 약 0.1초로 짧다고 가정 / (2) 서버 내 캐시는 1회 호출할때마다 비용이 안듬
-    private T[] heroes = (T[]) Array.newInstance(Hero.class, 0);
+    private List<T> heroes = new ArrayList<>();
     private final BasicHeroRepository<T> database;
 
     public CacheBasicHeroRepository(BasicHeroRepository<T> database) {
@@ -33,10 +35,8 @@ public abstract class CacheBasicHeroRepository<T extends Hero> implements CrudRe
             return null;
         }
         // (2.2) 데이터베이스에 있다면 캐시 내 저장하고 바로 반환
-        T[] newheroes = Arrays.copyOf(heroes, heroes.length + 1);
-        System.arraycopy(this.heroes, 0, newheroes, 0, this.heroes.length);
-        newheroes[this.heroes.length] = retrievedFromDatabase;
-        this.heroes = newheroes;
+
+        this.heroes.add(retrievedFromDatabase);
         System.out.println("- 캐시에는 없지만 데이터베이스에는 존재하여 캐시에 싱크 후 반환합니다 : " + name);
         return retrievedFromDatabase;
     }
