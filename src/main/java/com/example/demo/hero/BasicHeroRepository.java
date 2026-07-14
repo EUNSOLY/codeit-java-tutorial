22fa789276a5ba57b3d3122b3e552e39f5897cab
EUNSOLY <dmsthf9596@gmail.com>
2026-07-14 14:14:16 +0900

    실습6.  List -> Map으로 치환

--- src/main/java/com/example/demo/hero/BasicHeroRepository.java
+++ src/main/java/com/example/demo/hero/BasicHeroRepository.java
@@ -3,32 +3,31 @@ package com.example.demo.hero;
 import com.example.demo.hero.common.CrudRepository;
 import com.example.demo.hero.common.Hero;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
-public abstract class BasicHeroRepository<T extends Hero> implements CrudRepository<T> {
-    private List<T> heroes = new ArrayList<>();
+public abstract class BasicHeroRepository<T extends Hero> implements CrudRepository<String, T> {
+    private Map<String, T> heroes = new HashMap();
 
     public BasicHeroRepository(List<T> heroes) {
-        this.heroes.addAll(heroes);
+        for (T h : heroes) {
+            this.heroes.put(h.getName(), h);
+        }
+
     }
 
     @Override
-    public T findHeroByName(String name) {
-        for (T each : this.heroes) {
-            if (each.getName().equals(name)) {
-                return each;
-            }
+    public T findByKey(String name) {
+        if (this.heroes.containsKey(name)) {
+            return this.heroes.get(name);
         }
         return null;
     }
 
     @Override
     public void create(T hero) {
-        if (Objects.isNull(this.findHeroByName(hero.getName()))) {
+        if (Objects.isNull(this.findHeroByKey(hero.getName()))) {
             // null이 맞다면
-            this.heroes.add(hero);
+            this.heroes.put(hero.getName(), hero);
         } else {
             throw new RuntimeException("이미 존재하는 영웅의 이름입니다! 생성한 영웅 이름 : " + hero.getName());
         }
@@ -36,7 +35,7 @@ public abstract class BasicHeroRepository<T extends Hero> implements CrudReposit
 
     @Override
     public void delete(String name) {
-        T retrieve = findByName(name);
+        T retrieve = findByKey(name);
         if (Objects.isNull(retrieve)) {
             throw new RuntimeException("지우시려는 이름의 영웅이 기존에 없습니다! - 받은 영웅 객체의 이름 : " + name);
         }
--- src/main/java/com/example/demo/hero/common/CrudRepository.java
+++ src/main/java/com/example/demo/hero/common/CrudRepository.java
@@ -1,25 +1,25 @@
 package com.example.demo.hero.common;
 
-public interface CrudRepository<T> {
-    default void create(T entity) {
+public interface CrudRepository<K, V> {
+    default void create(V entity) {
         throw new RuntimeException("create 메서드는 구현되지 않았습니다 - 이건 인터페이스 내 디폴트 메서드입니다");
     }
 
-    default T update(T entity) {
+    default V update(V entity) {
         throw new RuntimeException("update 메서드는 구현되지 않았습니다 - 이건 인터페이스 내 디폴트 메서드입니다");
     }
 
-    default void delete(String name) {
+    default void delete(K key) {
         throw new RuntimeException("delete 메서드는 구현되지 않았습니다 - 이건 인터페이스 내 디폴트 메서드입니다");
     }
 
-    default T findHeroByName(String name) {
+    default V findHeroByKey(K key) {
         throw new RuntimeException("findHeroByName 메서드는 구현되지 않았습니다 - 이건 인터페이스 내 디폴트 메서드입니다");
     }
 
     ;
 
-    default T findByName(String name) {
+    default V findByKey(K key) {
         throw new RuntimeException("findByName 메서드는 구현되지 않았습니다 - 이건 인터페이스 내 디폴트 메서드입니다");
     }
 }
