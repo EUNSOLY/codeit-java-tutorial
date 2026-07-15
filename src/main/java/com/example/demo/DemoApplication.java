20cca490d56a9bbf32e0731638a74ff589ddffd2
EUNSOLY <dmsthf9596@gmail.com>
2026-07-15 14:04:29 +0900

    실습9. 최종연산자에 따라 반환되는 타입값 변경 연습 및 중간 연산자 연습

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,50 +1,56 @@
 package com.example.demo;
 
-import com.example.demo.hero.AgilityHero;
-import com.example.demo.hero.StrengthHero;
+import com.example.demo.game.Game;
 import com.example.demo.hero.common.Hero;
 
 import java.util.*;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 
 //@SpringBootApplication
 public class DemoApplication {
-    private static final Map<Integer, Player> PLAYER = new HashMap<>() {{
-        // Radiant 래디언트 진영
-        put(1, new Player(1, "Aaron", Side.RADIANT, 7, 12, 2, new AgilityHero("Slark")));
-        put(2, new Player(2, "Baron", Side.RADIANT, 2, 1, 6, new StrengthHero("Slardar")));
-        put(3, new Player(3, "Caron", Side.RADIANT, 4, 4, 4, new AgilityHero("Weaver")));
-        put(4, new Player(4, "Daron", Side.RADIANT, 8, 4, 4, new StrengthHero("Huskar")));
-        put(5, new Player(5, "Earon", Side.RADIANT, 1, 4, 4, new AgilityHero("Anti-Mage")));
-
-        // Dire 다이어 진영
-        put(6, new Player(6, "Faron", Side.DIRE, 0, 5, 9, new StrengthHero("Tiny")));
-        put(7, new Player(7, "Garon", Side.DIRE, 7, 4, 2, new StrengthHero("Dragon Knight")));
-        put(8, new Player(8, "Haron", Side.DIRE, 4, 3, 1, new StrengthHero("Night Stalker")));
-        put(9, new Player(9, "Iaron", Side.DIRE, 8, 2, 2, new AgilityHero("Phantom Lancer")));
-        put(10, new Player(10, "Jaron", Side.DIRE, 1, 8, 7, new AgilityHero("Drow Ranger")));
-
-    }};
-
     public static void main(String[] args) {
-        List<Player> players = new ArrayList<>(PLAYER.values());
-        Stream<Player> playerStream = players.stream();
+        List<Player> playerList = new ArrayList<>(Game.HISTORY.getFirst().getPlayers().values());
+
         System.out.println(" -- Stream.forEach -- ");
-        Stream<String> intermediate = playerStream
-                .peek(player -> System.out.println(" >> 첫번째 중간 연산자가 실행됩니다 << "))
+        Stream<Player> playerStream = playerList.stream();
+//        Stream<String> intermediate = playerStream
+//        List<Hero> intermediate = playerStream
+//        Set<Hero> intermediate = playerStream
+//        Map<String, Hero> intermediate = playerStream
+        String intermediate = playerStream
                 .filter(player -> player.getSide().equals(Side.RADIANT))
-                .peek(player -> System.out.println(" >> 두번째 중간 연산자가 실행됩니다 << "))
                 .filter(player -> player.getKill() >= 5)
                 .map(Player::getPickedHero)
-                .map(Hero::getName);
+                .map(Hero::getName)
+                .sorted(new Comparator<String>() {
+                    @Override
+                    public int compare(String o1, String o2) {
+                        return o1.compareTo(o2);
+                    }
+                })
+//                .sorted(new Comparator<Hero>() {
+//                    @Override
+//                    public int compare(Hero o1, Hero o2) {
+//                        return o1.getName().compareTo(o2.getName());
+//                    }
+//                })
+//                .map(Hero::getName)
+                .reduce("래디언트에서 5킬 이상을 달성한 영웅들은 : ",
+                        (accumulated, heroName) -> {
+                            return accumulated + ", " + heroName;
+                        });
 
-        System.out.println("필터가 완료되고, 실행시킵니다.");
-        intermediate.forEach(System.out::println);
+//                .collect(Collectors.toMap(
+//                        (hero -> hero.getName()),
+//                        (hero -> hero)
+//                ));
+//                .collect(Collectors.toSet())
+//                .map(Hero::getName)
+//                .forEach(System.out::println);
+//                .collect(Collectors.toList());
 
+        System.out.println(intermediate);
     }
 
 }
