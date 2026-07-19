bfe5f301d6b6bad7071e0d273a50ebd33a9169f0
EUNSOLY <dmsthf9596@gmail.com>
2026-07-19 14:50:10 +0900

    [학습] 14. 기본 요구사항 적용 (기본 CRUD)

--- src/main/java/com/sprint/mission/hero/DemoApplication.java
+++ src/main/java/com/sprint/mission/hero/DemoApplication.java
@@ -9,55 +9,32 @@ import com.sprint.mission.hero.entity.monster.Goblin;
 import com.sprint.mission.hero.entity.quest.Quest;
 import com.sprint.mission.hero.repository.*;
 
+import java.util.Comparator;
+import java.util.List;
+
 public class DemoApplication {
     public static void main(String[] args) {
 
-        // WarriorHero
+        // Hero
         WarriorHero heroAaron = new WarriorHero("Aaron", 10000, 1000);
         WarriorHero heroLeon = new WarriorHero("Leon", 8500, 950);
+        WarriorHero heroCaron = new WarriorHero("Caron", 8500, 950);
 
-        // MageHero
         MageHero heroMerlin = new MageHero("Merlin", 6500, 1800);
         MageHero heroLuna = new MageHero("Luna", 7000, 1700);
 
-        // ArcherHero
         ArcherHero heroRobin = new ArcherHero("Robin", 7500, 1400);
         ArcherHero heroSylvia = new ArcherHero("Sylvia", 7200, 1500);
 
-        WarriorHeroRepository warriorHeroRepository = new WarriorHeroRepository();
-        MageHeroRepository mageHeroRepository = new MageHeroRepository();
-        ArcherHeroRepository archerHeroRepository = new ArcherHeroRepository();
-
-        // Repo 저장
-        warriorHeroRepository.create(heroAaron);
-        warriorHeroRepository.create(heroLeon);
-
-        mageHeroRepository.create(heroMerlin);
-        mageHeroRepository.create(heroLuna);
-
-        archerHeroRepository.create(heroRobin);
-        archerHeroRepository.create(heroSylvia);
-
         // Monster
-        Goblin goblinKing = new Goblin("Goblin King");
-        Goblin goblinWarrior = new Goblin("Goblin Warrior");
-        Goblin goblinShaman = new Goblin("Goblin Shaman");
+        Goblin goblinKing = new Goblin("Goblin King", 100, 50);
+        Goblin goblinWarrior = new Goblin("Goblin Warrior", 80, 20);
+        Goblin goblinShaman = new Goblin("Goblin Shaman", 70, 10);
+        Goblin goblinTest = new Goblin("Goblin Test", 200, 120);
 
         Dragon redDragon = new Dragon("Red Dragon");
         Dragon blackDragon = new Dragon("Black Dragon");
         Dragon ancientDragon = new Dragon("Ancient Dragon");
-
-        MonsterRepository<Goblin> goblinMonsterRepository = new MonsterRepository<>();
-        MonsterRepository<Dragon> dragonMonsterRepository = new MonsterRepository<>();
-
-        // 몬스터 저장
-        goblinMonsterRepository.create(goblinKing);
-        goblinMonsterRepository.create(goblinWarrior);
-        goblinMonsterRepository.create(goblinShaman);
-
-        dragonMonsterRepository.create(redDragon);
-        dragonMonsterRepository.create(blackDragon);
-        dragonMonsterRepository.create(ancientDragon);
 
         // Quest
         Quest goblinHuntQuest = new Quest(
@@ -75,10 +52,129 @@ public class DemoApplication {
                 "붉은 드래곤을 처치하여 왕국을 구하세요."
         );
 
+        WarriorHeroRepository warriorHeroRepository = new WarriorHeroRepository();
+        MageHeroRepository mageHeroRepository = new MageHeroRepository();
+        ArcherHeroRepository archerHeroRepository = new ArcherHeroRepository();
+
+        MonsterRepository<Goblin> goblinMonsterRepository = new MonsterRepository<>();
+        MonsterRepository<Dragon> dragonMonsterRepository = new MonsterRepository<>();
+
         QuestRepository<Quest> questRepository = new QuestRepository<>();
+
+
+        // 히어로 저장
+        warriorHeroRepository.create(heroAaron);
+        warriorHeroRepository.create(heroLeon);
+        warriorHeroRepository.create(heroCaron);
+
+        mageHeroRepository.create(heroMerlin);
+        mageHeroRepository.create(heroLuna);
+
+        archerHeroRepository.create(heroRobin);
+        archerHeroRepository.create(heroSylvia);
+
+        // 몬스터 저장
+        goblinMonsterRepository.create(goblinKing);
+        goblinMonsterRepository.create(goblinWarrior);
+        goblinMonsterRepository.create(goblinShaman);
+        goblinMonsterRepository.create(goblinTest);
+
+        dragonMonsterRepository.create(redDragon);
+        dragonMonsterRepository.create(blackDragon);
+        dragonMonsterRepository.create(ancientDragon);
+
+        // 퀘스트 저장
         questRepository.create(goblinHuntQuest);
         questRepository.create(villageDefenseQuest);
         questRepository.create(dragonSlayerQuest);
+
+        //heroCaron
+//        System.out.println(warriorHeroRepository.find("Caron"));
+//        System.out.println("전체 조회");
+//        System.out.println(warriorHeroRepository.findAll());
+//        WarriorHero newHeroCaron = warriorHeroRepository.find("Caron");
+//        newHeroCaron.changeName("바보야");
+//        System.out.println("수정 후 전체 조회");
+//        System.out.println(warriorHeroRepository.findAll());
+//        warriorHeroRepository.delete("바보야");
+//        System.out.println("삭제 후 전체 조회");
+//        System.out.println(warriorHeroRepository.findAll());
+
+        // Goblin Test
+//        System.out.println(goblinMonsterRepository.find("Goblin Test"));
+//        System.out.println("전체 조회");
+//        System.out.println(goblinMonsterRepository.findAll());
+//        WarriorHero newGoblin = warriorHeroRepository.find("Goblin Test");
+//        newGoblin.changeName("바보야");
+//        System.out.println("수정 후 전체 조회");
+//        System.out.println(warriorHeroRepository.findAll());
+//        warriorHeroRepository.delete("바보야");
+//        System.out.println("삭제 후 전체 조회");
+//        System.out.println(warriorHeroRepository.findAll());
+
+        // 레벨 높은 순 조회
+        List<WarriorHero> sortLevelHero = warriorHeroRepository.findAll().stream()
+                .sorted(new Comparator<WarriorHero>() {
+                    @Override
+                    public int compare(WarriorHero o1, WarriorHero o2) {
+                        return o2.getLevel() - o1.getLevel();
+                    }
+                })
+                .toList();
+
+        System.out.println(sortLevelHero);
+
+
+        // 골드 높은 순 조회
+        List<WarriorHero> sortGoldHero = warriorHeroRepository.findAll().stream()
+                .sorted(new Comparator<WarriorHero>() {
+                    @Override
+                    public int compare(WarriorHero o1, WarriorHero o2) {
+                        return o2.getGold() - o1.getGold();
+                    }
+                })
+                .toList();
+        System.out.println(sortGoldHero);
+
+        // 레벨 10 이상
+        List<WarriorHero> sortTenLevelHero = warriorHeroRepository.findAll().stream()
+                .filter(h -> h.getLevel() >= 10)
+                .toList();
+        System.out.println(sortTenLevelHero);
+
+        // 고블린 공격력 높은 순서
+        List<Goblin> goblinSortAttackList = goblinMonsterRepository.findAll().stream()
+                .sorted(new Comparator<Goblin>() {
+                    @Override
+                    public int compare(Goblin o1, Goblin o2) {
+                        return o2.getAttack() - o1.getAttack();
+                    }
+                }).toList();
+
+
+        // 고블린 보상 높은 순서
+        List<Goblin> goblinSortGoldList = goblinMonsterRepository.findAll().stream()
+                .sorted(new Comparator<Goblin>() {
+                    @Override
+                    public int compare(Goblin o1, Goblin o2) {
+                        return o2.getRewardGold() - o1.getRewardGold();
+                    }
+                }).toList();
+
+        // 완료된 퀘스트 조회
+        List<Quest> questResultList = questRepository.findAll().stream()
+                .filter(Quest::isCompleted)
+                .toList();
+
+        // 미완료 퀘스트 조회
+        List<Quest> questNotResultList = questRepository.findAll().stream()
+                .filter(q -> !q.isCompleted())
+                .toList();
+
+
+        System.out.println(warriorHeroRepository.findAll());
+        System.out.println(goblinMonsterRepository.findAll());
+        System.out.println(questRepository.findAll());
 
     }
 
--- src/main/java/com/sprint/mission/hero/entity/monster/Goblin.java
+++ src/main/java/com/sprint/mission/hero/entity/monster/Goblin.java
@@ -3,10 +3,10 @@ package com.sprint.mission.hero.entity.monster;
 import com.sprint.mission.hero.common.MonsterAbstract;
 
 public class Goblin extends MonsterAbstract {
-    public Goblin(String name) {
+    public Goblin(String name, int attack, int gold) {
         super.changeName(name);
         super.changeHp(1500); // 고블린 체력
-        super.changeAttack(150); // 고블린 공격력
-        super.changeRewardGold(150); // 고블린 처치 시 획든 골드
+        super.changeAttack(attack); // 고블린 공격력
+        super.changeRewardGold(gold); // 고블린 처치 시 획든 골드
     }
 }
