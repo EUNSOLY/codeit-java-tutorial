d9ba241e3acacbaf74d1fb2ad5e1edac1d8c27a2
EUNSOLY <dmsthf9596@gmail.com>
2026-07-19 12:10:50 +0900

    [학습] 10. 몬스터, 퀘스트 repo 클래스 구현체 생성
    - 제네릭을 통해 1개 구현채를 가지고 여러 타입의 몬스터, 퀘스트 저장 할 수 있도록 적용
    - 퀘스트의 경우 내부 제목과 설명만 다를 뿐 다른게 없어서 1개의 레포에 같이 생성할 수있음

---
+++ src/main/java/com/sprint/mission/hero/repository/MonsterRepository.java
@@ -0,0 +1,7 @@
+package com.sprint.mission.hero.repository;
+
+import com.sprint.mission.hero.common.AbstractMonsterRepository;
+import com.sprint.mission.hero.common.MonsterAbstract;
+
+public class MonsterRepository<T extends MonsterAbstract> extends AbstractMonsterRepository<T> {
+}
---
+++ src/main/java/com/sprint/mission/hero/repository/QuestRepository.java
@@ -0,0 +1,8 @@
+package com.sprint.mission.hero.repository;
+
+import com.sprint.mission.hero.common.AbstractQuestRepository;
+import com.sprint.mission.hero.common.QuestAbstract;
+
+public class QuestRepository<T extends QuestAbstract> extends AbstractQuestRepository<T> {
+
+}
